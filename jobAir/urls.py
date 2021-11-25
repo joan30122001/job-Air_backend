23882d2139a0214b49091b06ad5fe98b34014c7c
DJILA YVAN <yvanitchedjila@gmail.com>
2021-11-25 16:57:23 +0100

    ajout modif

--- jobAir/urls.py
+++ jobAir/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/job', include('job.urls')),
-    path('api/user', include('user.urls')),
+    path('api/job/', include('job.urls')),
+    path('api/user/', include('user.urls')),
 ]
--- user/models.py
+++ user/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class User(models.Model):
+class User(AbstractUser):
     pseudo = models.CharField(max_length=50),
     email = models.EmailField(max_length=254),
     password = models.CharField(max_length=50)
